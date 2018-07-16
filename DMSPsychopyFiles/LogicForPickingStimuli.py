593119da3e248917df5e404217bc44df8998e06a
steffejr <steffener@gmail.com>
2018-07-16 16:59:02 -0400

    Changed the logic for the DMS task

--- DMSPsychopyFiles/LogicForPickingStimuli.py
+++ DMSPsychopyFiles/LogicForPickingStimuli.py
@@ -35,6 +35,8 @@ for i in range(0,9,1):
     if bool(Probe):
         # Yes, the probe is in the set
         CurrentProbe = CurrentStim[np.random.permutation(len(CurrentStim))[0]]
+        CurrentProbe = CurrentProbe.lower()
+        corr = 'left'
     else:
         # Remove the current stim letters from the available letter set
         for j in CurrentStim:
@@ -42,6 +44,33 @@ for i in range(0,9,1):
         # remove empty locations from the list
         tempLetterList = [x for x in tempLetterList if x] 
         CurrentProbe = tempLetterList[np.random.permutation(len(tempLetterList))[0]]
+        corr = 'down'
     LastTrial = CurrentStim + CurrentProbe
-    print(CurrentStim+'   '+CurrentProbe)
+    InStim = MapLettersToScreen(CurrentStim)
+    
+    print(InStim+'   '+CurrentProbe)
     CurrentLoad += 1
+    
+def MapLettersToScreen(Stim):
+    NLet = len(Stim)
+    if NLet == 1:
+        Output = '****'+Stim+'****'
+    elif NLet == 2:
+        Output = '***'+Stim[0]+'*'+Stim[1]+'***'
+    elif NLet == 3:
+        Output = '***'+Stim+'***'
+    elif NLet == 4:
+        Output = Stim[0]+'*'+Stim[1]+'***'+Stim[2]+'*'+Stim[3]
+    elif NLet == 5:
+        Output = Stim[0]+'*'+Stim[1]+'*'+Stim[2]+'*'+Stim[3]+'*'+Stim[4]
+    elif NLet == 6:
+        Output = Stim[0:3]+'***'+Stim[3:6]
+    elif NLet == 7:
+        Output = Stim[0:3]+'*'+Stim[3]+'*'+Stim[4:7]
+    elif NLet == 8:
+        Output = Stim[0:4]+'*'+Stim[4:8]
+    else:
+        Output = Stim
+    return(Output)
+        
+        
