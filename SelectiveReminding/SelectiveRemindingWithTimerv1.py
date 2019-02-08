087b2c527311d2bfec3fbc2e3fb09ab721df762f
Jason Steffener NCMLab2 <steffener@gmail.com>
2019-02-08 11:32:51 -0500

    minor removal

--- SelectiveReminding/SelectiveRemindingWithTimerv1.py
+++ SelectiveReminding/SelectiveRemindingWithTimerv1.py
@@ -156,13 +156,6 @@ textThankyou = visual.TextStim(win=win, name='Thanks',
     color=u'white', colorSpace='rgb', opacity=1,
     depth=0.0);
     
-# on screen timer
-Clock = visual.TextStim(win=win, name='Clock',
-    text='default text',
-    font='Arial',
-    pos=(400, -400), height=30, wrapWidth=None, ori=0, 
-    color='white', colorSpace='rgb', opacity=1,
-    depth=-19.0);
     
 # Create some handy timers
 globalClock = core.Clock()  # to track the time since experiment started
