df60d34cd13e58a1fab1889908d723b9f7c96053
Jason Steffener-LabLaptop <steffener@gmail.com>
2019-04-18 13:26:21 -0400

    data folder

--- ConfigFiles/NeuropsychDataFolder.py
+++ ConfigFiles/NeuropsychDataFolder.py
@@ -1,1 +1,1 @@
-NeuropsychDataFolder = '/Users/jason/Dropbox/SteffenerColumbia/Projects/MyProjects/NeuralCognitiveMapping/NeuroPsychData'
+NeuropsychDataFolder = '/Users/jasonsteffener/Dropbox/steffenercolumbia/Projects/MyProjects/NeuralCognitiveMapping/NeuroPsychData'
