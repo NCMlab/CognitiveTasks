ea9c2efffa7355d95181e3fb22289b3615daf04f
Jason Steffener NCMLab2 <steffener@gmail.com>
2019-07-25 09:53:49 -0400

    trying and failing to get silent clicking for SRT

--- SelectiveReminding/Tester2.py
+++ SelectiveReminding/Tester2.py
@@ -1,15 +1,14 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.1.0),
-    on Wed Jul 24 19:50:10 2019
+    on Wed Jul 24 19:45:06 2019
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
     Peirce, JW (2009) Generating stimuli for neuroscience using PsychoPy.
         Frontiers in Neuroinformatics, 2:10. doi: 10.3389/neuro.11.010.2008
 """
-
 from __future__ import absolute_import, division
 from psychopy import locale_setup, sound, gui, visual, core, data, event, logging, clock
 from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
@@ -73,7 +72,10 @@ text = visual.TextStim(win=win, name='text',
     color='white', colorSpace='rgb', opacity=1, 
     languageStyle='LTR',
     depth=0.0);
+    
 mouse = event.Mouse(win=win)
+
+
 x, y = [None, None]
 mouse.mouseClock = core.Clock()
 polygon = visual.Rect(
@@ -105,66 +107,37 @@ mouse.clicked_name = []
 gotValidClick = False  # until a click is received
 # keep track of which components have finished
 trialComponents = [text, mouse, polygon]
-for thisComponent in trialComponents:
-    thisComponent.tStart = None
-    thisComponent.tStop = None
-    thisComponent.tStartRefresh = None
-    thisComponent.tStopRefresh = None
-    if hasattr(thisComponent, 'status'):
-        thisComponent.status = NOT_STARTED
 
 # -------Start Routine "trial"-------
 while continueRoutine:
     # get current time
     t = trialClock.getTime()
     frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
-    # update/draw components on each frame
-    
-    # *text* updates
-    if t >= 0.0 and text.status == NOT_STARTED:
-        # keep track of start time/frame for later
-        text.tStart = t  # not accounting for scr refresh
-        text.frameNStart = frameN  # exact frame index
-        win.timeOnFlip(text, 'tStartRefresh')  # time at next scr refresh
-        text.setAutoDraw(True)
-    # *mouse* updates
-    if t >= 0.0 and mouse.status == NOT_STARTED:
-        # keep track of start time/frame for later
-        mouse.tStart = t  # not accounting for scr refresh
-        mouse.frameNStart = frameN  # exact frame index
-        win.timeOnFlip(mouse, 'tStartRefresh')  # time at next scr refresh
-        mouse.status = STARTED
-        mouse.mouseClock.reset()
-        prevButtonState = [0, 0, 0]  # if now button is down we will treat as 'new' click
-    if mouse.status == STARTED:  # only update if started and not finished!
-        x, y = mouse.getPos()
-        mouse.x.append(x)
-        mouse.y.append(y)
-        buttons = mouse.getPressed()
-        mouse.leftButton.append(buttons[0])
-        mouse.midButton.append(buttons[1])
-        mouse.rightButton.append(buttons[2])
-        mouse.time.append(mouse.mouseClock.getTime())
-        buttons = mouse.getPressed()
-        if buttons != prevButtonState:  # button state changed?
-            prevButtonState = buttons
-            if sum(buttons) > 0:  # state changed to a new click
-                # check if the mouse was inside our 'clickable' objects
-                gotValidClick = False
-                for obj in [polygon]:
-                    if obj.contains(mouse):
-                        gotValidClick = True
-                        mouse.clicked_name.append(obj.name)
-                if gotValidClick:  # abort routine on response
-                    continueRoutine = False
-    
-    # *polygon* updates
-    if t >= 0.0 and polygon.status == NOT_STARTED:
-        # keep track of start time/frame for later
-        polygon.tStart = t  # not accounting for scr refresh
-        polygon.frameNStart = frameN  # exact frame index
-        win.timeOnFlip(polygon, 'tStartRefresh')  # time at next scr refresh
-        polygon.setAutoDraw(True)
+
+    text.setAutoDraw(True)
+    prevButtonState = [0, 0, 0] 
+    x, y = mouse.getPos()
+    mouse.x.append(x)
+    mouse.y.append(y)
+    buttons = mouse.getPressed()
+    mouse.leftButton.append(buttons[0])
+    mouse.midButton.append(buttons[1])
+    mouse.rightButton.append(buttons[2])
+    mouse.time.append(mouse.mouseClock.getTime())
+    buttons = mouse.getPressed()
+    if buttons != prevButtonState:  # button state changed?
+        prevButtonState = buttons
+        if sum(buttons) > 0:  # state changed to a new click
+            # check if the mouse was inside our 'clickable' objects
+            gotValidClick = False
+            for obj in [polygon]:
+                if obj.contains(mouse):
+                    gotValidClick = True
+                    mouse.clicked_name.append(obj.name)
+            if gotValidClick:  # abort routine on response
+                continueRoutine = False
+
+    polygon.setAutoDraw(True)
     
     # check for quit (typically the Esc key)
     if endExpNow or keyboard.Keyboard().getKeys(keyList=["escape"]):
@@ -187,8 +160,7 @@ while continueRoutine:
 for thisComponent in trialComponents:
     if hasattr(thisComponent, "setAutoDraw"):
         thisComponent.setAutoDraw(False)
-thisExp.addData('text.started', text.tStartRefresh)
-thisExp.addData('text.stopped', text.tStopRefresh)
+
 # store data for thisExp (ExperimentHandler)
 thisExp.addData('mouse.x', mouse.x)
 thisExp.addData('mouse.y', mouse.y)
@@ -197,11 +169,7 @@ thisExp.addData('mouse.midButton', mouse.midButton)
 thisExp.addData('mouse.rightButton', mouse.rightButton)
 thisExp.addData('mouse.time', mouse.time)
 thisExp.addData('mouse.clicked_name', mouse.clicked_name)
-thisExp.addData('mouse.started', mouse.tStartRefresh)
-thisExp.addData('mouse.stopped', mouse.tStopRefresh)
 thisExp.nextEntry()
-thisExp.addData('polygon.started', polygon.tStartRefresh)
-thisExp.addData('polygon.stopped', polygon.tStopRefresh)
 # the Routine "trial" was not non-slip safe, so reset the non-slip timer
 routineTimer.reset()
 
