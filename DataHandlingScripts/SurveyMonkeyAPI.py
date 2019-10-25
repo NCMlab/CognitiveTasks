8c42be311c2c093d0d460277078c6f95af5908a4
steffejr <steffener@gmail.com>
2019-10-25 16:12:19 -0400

    working on this

--- DataHandlingScripts/SurveyMonkeyAPI.py
+++ DataHandlingScripts/SurveyMonkeyAPI.py
@@ -1,15 +1,32 @@
-from client import Client
-# If you do not have access_token, run
-CLIENT_ID = 'D_kzOb9fSo2zC64qMNt7Xg'
-CLIENT_SECRET = '32248023616401215046166317729204720865'
-REDIRECT_URI = 'https://www.surveymonkey.com'
-cl = client.Client(
-    client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, access_token=None)
-# If you have access_token, run
+# https://digitalborn.org/get-survey-monkey-data-using-the-rest-api/
 survey_id = 190091421
-client=Client(
-    client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, access_token=ACCESS_TOKEN)
     
     
     
 curl -i -X GET -H "Authorization:bearer -MMu4iKFM1vKiFeMgM7kpEVLeQQ8AqhD-BnE-3hBeNHrwupDZfsyE-sIZ9.cmyFKGnWrHHr0g0k2exW1BS.d0qzzCd.-Msqn9VqeVCN4RHkKGYaWC2QOYa1hvwQ9VXVh" -H "Content-Type": "application/json" https://api.surveymonkey.com/v3/surveys/190091421/responses/bulk
+
+
+import requests
+import json
+
+client = requests.session()
+
+headers = {
+    "Authorization": "bearer %s" % "MMu4iKFM1vKiFeMgM7kpEVLeQQ8AqhD-BnE-3hBeNHrwupDZfsyE-sIZ9.cmyFKGnWrHHr0g0k2exW1BS.d0qzzCd.-Msqn9VqeVCN4RHkKGYaWC2QOYa1hvwQ9VXVh",
+    "Content-Type": "application/json"
+}
+
+data = {}
+
+HOST = "https://api.surveymonkey.net"
+#SURVEY_LIST_ENDPOINT = "/v3/surveys/%s/responses/%s/details" %("85160626","161")
+SURVEY_LIST_ENDPOINT = "/v3/surveys/190091421/responses"
+
+uri = "%s%s" % (HOST, SURVEY_LIST_ENDPOINT)
+
+response = client.get(uri, headers=headers, data=json.dumps(data))
+
+response_json = response.json()
+#survey_list = response_json["data"]["surveys"]
+
+print(response_json)
