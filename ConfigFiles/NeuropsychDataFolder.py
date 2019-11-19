157c5b494517f78a3bdd9b8297aa660bade96c5b
Jason Steffener NCMLab2 <steffener@gmail.com>
2019-11-19 09:36:31 -0500

    Update NeuropsychDataFolder.py

--- ConfigFiles/NeuropsychDataFolder.py
+++ ConfigFiles/NeuropsychDataFolder.py
@@ -1,2 +1,1 @@
-#NeuropsychDataFolder = '/Volumes/GoogleDrive/Shared drives/NCMLab/NCM002-MRIStudy/Data/NeuroPsych'
-NeuropsychDataFolder = '/media/jsteffen/Data001/NCMTeamDrive/NCMLab/NCM002-MRIStudy/Data/NeuroPsych'
+NeuropsychDataFolder = '/Volumes/GoogleDrive/Shared drives/NCMLab/NCM002-MRIStudy/Data/NeuroPsych'
