aacf4969baf8ad510f4b3e04a47d7079d2f14109
Jason Steffener, Lab 1 <steffener@gmail.com>
2020-08-18 12:18:00 -0400

    added RT task

--- ConfigFiles/NeuropsychDataFolder.py
+++ ConfigFiles/NeuropsychDataFolder.py
@@ -1,5 +1,6 @@
 
-NeuropsychDataFolder = '/Volumes/GoogleDrive/Shared drives/NCMLab/NCM002-MRIStudy/Data/NeuroPsych'
+# NeuropsychDataFolder = '/Volumes/GoogleDrive/Shared drives/NCMLab/NCM002-MRIStudy/Data/NeuroPsych'
 
 #NeuropsychDataFolder = '/media/jsteffen/Data001/NCMTeamDrive/NCMLab/NCM002-MRIStudy/Data/NeuroPsych'
+NeuropsychDataFolder = '/media/jsteffener/Data001/NCMLab/TeamDrive/NCMLab/NCM002-MRIStudy/Data/NeuroPsych'
 
